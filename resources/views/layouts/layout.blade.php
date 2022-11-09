68c0fc557420c85da6be83852fa42c9eb08b7f64
ibrahimstihi <ibrahimstihi2015@gmail.com>
2022-11-09 15:06:14 +0100

    message

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -26,70 +26,6 @@
 <body>
         <!-- Topbar Start -->
         <div class="container-fluid">
-            <div class="row bg-secondary py-1 px-xl-5">
-                <div class="col-lg-6 d-none d-lg-block">
-                    <div class="d-inline-flex align-items-center h-100">
-                        <a class="text-body mr-3" href="">About</a>
-                        <a class="text-body mr-3" href="">Contact</a>
-                        <a class="text-body mr-3" href="">Help</a>
-                        <a class="text-body mr-3" href="">FAQs</a>
-                    </div>
-                </div>
-                <div class="col-lg-6 text-center text-lg-right">
-                    <div class="d-inline-flex align-items-center">
-                        
-                            
-
-                        @guest
-                          <div class="btn-group">
-                            <button type="button" class="btn btn-sm btn-light dropdown-toggle" data-toggle="dropdown">My Account</button>
-                            <div class="dropdown-menu dropdown-menu-right">
-                            <a class="dropdown-item" href="{{ route('signin') }}" >Sign in</a>
-                            <a class="dropdown-item" href="{{ route('signup') }}" >Sign up</a>
-                          </div>
-                        @else
-                        <a href="#" role="button">
-                            {{ Auth::user()->name }}
-                        </a>
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                               onclick="event.preventDefault();
-                                             document.getElementById('logout-form').submit();">
-                                <i class="fa fa-sign-out"></i>
-                            </a>
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                @csrf
-                            </form>
-                        @endguest
-                        </div>
-                        {{-- <div class="btn-group mx-2">
-                            <button type="button" class="btn btn-sm btn-light dropdown-toggle" data-toggle="dropdown">USD</button>
-                            <div class="dropdown-menu dropdown-menu-right">
-                                <button class="dropdown-item" type="button">EUR</button>
-                                <button class="dropdown-item" type="button">GBP</button>
-                                <button class="dropdown-item" type="button">CAD</button>
-                            </div>
-                        </div> 
-                        <div class="btn-group">
-                            <button type="button" class="btn btn-sm btn-light dropdown-toggle" data-toggle="dropdown">EN</button>
-                            <div class="dropdown-menu dropdown-menu-right">
-                                <button class="dropdown-item" type="button">FR</button>
-                                <button class="dropdown-item" type="button">AR</button>
-                                <button class="dropdown-item" type="button">RU</button>
-                            </div>
-                        </div> --}}
-                    </div>
-                    <div class="d-inline-flex align-items-center d-block d-lg-none">
-                        <a href="" class="btn px-0 ml-2">
-                            <i class="fas fa-heart text-dark"></i>
-                            <span class="badge text-dark border border-dark rounded-circle" style="padding-bottom: 2px;">3</span>
-                        </a>
-                        <a href="" class="btn px-0 ml-2">
-                            <i class="fas fa-shopping-cart text-dark"></i>
-                            <span class="badge text-dark border border-dark rounded-circle" style="padding-bottom: 2px;">2</span>
-                        </a>
-                    </div>
-                </div>
-            </div>
             <div class="row align-items-center bg-light py-3 px-xl-5 d-none d-lg-flex">
                 <div class="col-lg-4">
                     <a href="/" class="text-decoration-none">
@@ -113,6 +49,7 @@
                     <p class="m-0">Customer Service</p>
                     <h5 class="m-0">+012 345 6789</h5>
                 </div>
+                
             </div>
         </div>
         <!-- Topbar End -->
@@ -169,16 +106,40 @@
                                 </div>
                                 <a href="contact.html" class="nav-item nav-link">Contact</a>
                             </div>
-                            <div class="navbar-nav ml-auto py-0 d-none d-lg-block">
-                                <a href="" class="btn px-0">
-                                    <i class="fas fa-heart text-primary"></i>
-                                    <span class="badge text-secondary border border-secondary rounded-circle" style="padding-bottom: 2px;">0</span>
-                                </a>
+                            
+                            <div class="navbar-nav ml-auto py-0 d-none d-lg-block" style="margin: 10px">
                                 <a href="{{ route('cart.index') }}" class="btn px-0 ml-3">
-                                    <i class="fas fa-shopping-cart text-primary"></i>
-                                    <span class="badge text-secondary border border-secondary rounded-circle" style="padding-bottom: 2px;">{{ $items->count() }}</span>
+                                    <i class="fas fa-shopping-cart text-secondary"></i>
+                                    <span class="badge  border border-secondary rounded-circle text-primary" style="padding-bottom: 2px;" >{{ $items->count() }}</span>
                                 </a>
                             </div>
+                            @guest
+                                <div class="dropdown show">
+                                
+                                    <a class="btn btn-primary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        
+                                        My Account
+                                         <i class="fa fa-caret-down" aria-hidden="true"></i>
+                                    </a>
+                                    <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                                        <a class="dropdown-item" href="{{ route('signin') }}" >Sign in</a>
+                                        <a class="dropdown-item" href="{{ route('signup') }}" >Sign up</a>
+                                    </div>
+                                </div>
+                            @else
+                        
+                                <a class="text-secondary">
+                                    {{ Auth::user()->name }}
+                                </a>
+                                   
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" >
+                                    @csrf
+                                    <button type="submit" class="btn btn-link">
+                                       <i class="fa fa-sign-out text-secondary"></i>
+                                    </button>
+                                </form>
+                            @endguest
+                        </div>
                         </div>
                     </nav>
                 </div>
